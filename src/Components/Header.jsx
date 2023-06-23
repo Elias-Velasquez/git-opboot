1fe63f1215edab89a279f4653acdc783b4b771a5
Elias Velasquez <eliasv.cabj@gmail.com>
2023-06-23 17:07:18 -0500

    portfolio header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    
+        <div className='Header w-full flex flex-col justify-center items-center'>
+            <h2 className='NameHeader text-center'>ELIAS VELASQUEZ</h2>
+            <h4 className='DevHeader text-center'>Desarrollador Fullstack</h4>
+        </div>
+    
+  )
+}
+
+export default Header
